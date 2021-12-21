c12fab471d14515828771e9faa5bf14847f0e367
charliemidtlyng <charlie.midtlyng@bekk.no>
2021-12-21 10:39:16 +0100

    Riktig project id mot sanity

--- src/main/kotlin/no/nav/familie/env/Env.kt
+++ src/main/kotlin/no/nav/familie/env/Env.kt
@@ -5,6 +5,4 @@ val DB_PASSWORD: String = System.getenv("db_PASSWORD")
 val DB_HOST: String = System.getenv("db_HOST")
 val DB_DATABASE: String = System.getenv("db_DATABASE")
 val DB_PORT: Int = System.getenv("db_PORT").toInt()
-val CORS_ALLOWED_HOST: String = System.getenv("CORS_ALLOWED_HOST")
-val BACKEND_PORT: Int = System.getenv("BACKEND_PORT").toInt()
-val SANITY_PROJECT_ID: String = "tralalallaa" // TODO: Sett riktig sanity id
+const val SANITY_PROJECT_ID: String = "avzz8jwq"
