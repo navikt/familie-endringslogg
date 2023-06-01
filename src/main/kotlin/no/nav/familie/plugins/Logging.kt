5942d1e567d0216667e02701b0fa6ebb78a0c1d5
Johan Blomgren <blommish@users.noreply.github.com>
2023-06-01 12:19:23 +0200

    Legger på duration på innkommende kall

--- src/main/kotlin/no/nav/familie/plugins/Logging.kt
+++ src/main/kotlin/no/nav/familie/plugins/Logging.kt
@@ -1,11 +1,18 @@
 package no.nav.familie.plugins
 
 import io.ktor.server.application.Application
+import io.ktor.server.application.ApplicationCallPipeline
+import io.ktor.server.application.call
 import io.ktor.server.application.install
 import io.ktor.server.plugins.callid.CallId
 import io.ktor.server.plugins.callloging.CallLogging
 import io.ktor.server.request.ApplicationRequest
 import io.ktor.server.request.header
+import io.ktor.server.request.httpMethod
+import io.ktor.server.request.path
+import io.ktor.util.AttributeKey
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
 import java.util.UUID
 
 const val RANDOM_USER_ID_COOKIE_NAME = "RUIDC"
@@ -16,11 +23,19 @@ object MDCConstants {
     const val CALL_ID = "callId"
 }
 
+val CALL_START_TIME = AttributeKey<LocalDateTime>("CallStartTime")
+
 fun Application.configureLogging() {
     install(CallId) {
         retrieve { resolveCallId(it.request) }
         replyToHeader("Nav-Call-Id")
     }
+
+    intercept(ApplicationCallPipeline.Setup) {
+        // intercept før routing
+        call.attributes.put(CALL_START_TIME, LocalDateTime.now())
+    }
+
     install(CallLogging) {
         disableDefaultColors()
         mdc(MDCConstants.CONSUMER_ID) { call ->
@@ -32,6 +47,17 @@ fun Application.configureLogging() {
         mdc(MDCConstants.CALL_ID) { call ->
             resolveCallId(call.request)
         }
+
+        format { call ->
+            val status = call.response.status()
+            val httpMethod = call.request.httpMethod.value
+            val path = call.request.path()
+            val duration = when (val startTime = call.attributes.getOrNull(CALL_START_TIME)) {
+                null -> "?ms"
+                else -> "${startTime.until(LocalDateTime.now(), ChronoUnit.MILLIS)}ms"
+            }
+            "$httpMethod - $path - (${status?.value}). Dette tok $duration"
+        }
     }
 }
 
@@ -40,7 +66,7 @@ private val NAV_CALL_ID_HEADER_NAMES =
         "Nav-Call-Id",
         "Nav-CallId",
         "Nav-Callid",
-        "X-Correlation-Id"
+        "X-Correlation-Id",
     )
 
 private fun resolveCallId(request: ApplicationRequest): String {
